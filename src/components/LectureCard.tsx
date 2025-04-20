8fd5d291f1e1497716f6331855e4f7bdc661ca7c
ygvbhy <ygvbhy@naver.com>
2025-04-20 16:28:29 +0900

    Perf: New btn add

--- src/components/LectureCard.tsx
+++ src/components/LectureCard.tsx
@@ -4,9 +4,14 @@ export default function LectureCard({ lecture }: { lecture: LectureType }) {
   return (
     <>
       <div className="bg-gray-100">
-        <div className="h-full bg-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg">
+        <div className="h-full bg-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg relative">
           <a href={lecture.link} target="_blank" rel="noreferrer">
             <div className="rounded-lg border border-gray-200 shadow-md">
+              {lecture.is_new && (
+                <span className="absolute right-0 top-2 bg-red-800 text-red-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                  NEW
+                </span>
+              )}
               <img
                 src={lecture.image}
                 alt={lecture.title}
